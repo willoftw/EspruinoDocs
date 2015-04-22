dcdbf9df1348d83a3b3c35a3526adeb24d23a054
willoftw <william.olner@gmail.com>
2015-04-22 13:57:16 +0100

    SSD1306 debug

--- devices/SSD1306.js
+++ devices/SSD1306.js
@@ -1,8 +1,8 @@
 /* Copyright (c) 2014 Sam Sykes, Gordon Williams. See the file LICENSE for copying permission. */
-/* 
+/*
 Module for the SSD1306 OLED controller in the Crius CO-16
 
-```  
+```
 
  */
 // registers
@@ -20,7 +20,7 @@ var C = {
 // export
 exports.connect = function(i2c, callback) {
  var oled = Graphics.createArrayBuffer(128,64,1,{vertical_byte : true});
- 
+
  // configure the OLED
  i2c.writeTo(C.OLED_ADDRESS, [C.OLED_CMD, 0xAE]);        // display off
  i2c.writeTo(C.OLED_ADDRESS, [C.OLED_CMD, 0x2E]);        // deactivate scrolling
@@ -30,15 +30,15 @@ exports.connect = function(i2c, callback) {
  i2c.writeTo(C.OLED_ADDRESS, [C.OLED_CMD, 0x00]);        // reset paging
  i2c.writeTo(C.OLED_ADDRESS, [C.OLED_CMD, 0xA1]);        // orientation
  i2c.writeTo(C.OLED_ADDRESS, [C.OLED_CMD, 0xC8]);        // left > right
- 
+
  // turn on the display
  i2c.writeTo(C.OLED_ADDRESS, [C.OLED_CMD, 0xAF]);
- 
+
  // if there is a callback, call it now(ish)
  if (callback !== undefined) setTimeout(callback, 10);
-  
+
  // write to the screen
- oled.flip = function() { 
+ oled.flip = function() {
    // set how the data is to be sent (whole screen)
    i2c.writeTo(C.OLED_ADDRESS, [C.OLED_CMD, 0x21]); // columns
    i2c.writeTo(C.OLED_ADDRESS, [C.OLED_CMD, 0]);
@@ -46,19 +46,19 @@ exports.connect = function(i2c, callback) {
    i2c.writeTo(C.OLED_ADDRESS, [C.OLED_CMD, 0x22]); // rows
    i2c.writeTo(C.OLED_ADDRESS, [C.OLED_CMD, 0]);
    i2c.writeTo(C.OLED_ADDRESS, [C.OLED_CMD, 7]);
- 
+
    var chunk = new Uint8Array(C.OLED_CHUNK+1);
+   console.log("created chunk array");
 
    chunk[0] = C.OLED_CHAR;
    for (var p=0; p<C.OLED_LENGTH; p+=C.OLED_CHUNK) {
+     console.log("writing to oled");
      chunk.set(new Uint8Array(this.buffer,p,C.OLED_CHUNK), 1);
      i2c.writeTo(C.OLED_ADDRESS, chunk);
-   }   
+   }
   };
- 
+
  // return graphics
- return oled;  
+ return oled;
 
 };
-
-
